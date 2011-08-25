3d241351b50a08d3def63c144c2e6b0842bf497d
mgiulio <giulio.mainardi@gmail.com>
2011-08-25 16:44:11 +0200

    First pages commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(window).load(function() {
+	var
+		$slides = $('#slides'),
+		$frame = $('<div id="frame"></div>'),
+		$prev = $('<a class="nav_arrow" id="prev" href="#">prev</a>'),
+		$next = $('<a class="nav_arrow" id="next" href="#">next</a>'),
+		$screen = $('<div id="screen"></div>')
+			.append($prev, $next)
+			.hover(function() { $slides.cycle('toggle'); $prev.toggle(); $next.toggle(); }),
+		$pager = $('<div id="pager"></div>').hover(
+			function() { $slides.cycle('toggle'); $prev.toggle(); $next.toggle(); }
+		)
+	; 
+	
+	$slides.after($frame);
+	$frame.append($screen, $pager);
+	$screen.append($slides);
+	
+	$slides.cycle({
+		fx: 'scrollHorz',
+		timeout: 4000,
+		speed: 500,
+		prev: '#prev',
+		next: '#next',
+		pager: '#pager'
+	});
+});
